f8570395beac032b6e5951a0f98bc6cd4b04bd30
Yaroslav <yaroslavgolinsiy@gmail.com>
2025-07-17 13:20:51 +0700

    Fix: reorganize package structure

--- Package.swift
+++ Package.swift
@@ -1,31 +1,24 @@
-//
-//  Package.swift
-//  STDesignSystem
-//
-//  Created by Yaroslav Golinskiy on 17/07/2025.
-//
 
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
     name: "STDesignSystem",
     platforms: [
-        .iOS(.v14)
+        .iOS(.v15)
     ],
     products: [
         .library(
             name: "STDesignSystem",
             targets: ["STDesignSystem"]),
     ],
-    dependencies: [
-        // Поки що немає зовнішніх залежностей
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "STDesignSystem",
             dependencies: []),
         .testTarget(
             name: "STDesignSystemTests",
-            dependencies: ["STDesignSystem"]),
+            dependencies: ["STDesignSystem"])
     ]
 )
